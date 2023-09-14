c01b4afbdc1364ae8fd6c7a4aceb5b7acb79cfb4
Leomir <leomir2002@gmail.com>
2023-09-14 16:08:33 +0300

    Physics baby

--- core/src/com/mygdx/game/Level/Player.java
+++ core/src/com/mygdx/game/Level/Player.java
@@ -6,38 +6,68 @@ import com.badlogic.gdx.InputAdapter;
 import com.badlogic.gdx.InputProcessor;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.Polygon;
 import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.physics.box2d.*;
 
 public class Player extends InputAdapter {
-    float position_x = 200;
-    float position_y = 300;
-    float PlayersSpeed = 55.0f;
-    Vector2 vector2;
+
+    private static final float MAX_VELOCITY = 800f;
+    PolygonShape playerShape;
+    FixtureDef fixtureDef;
+    private Body body;
+    private float width = 32;
+    private float height = 32;
+    public float position_x = 200;
+    public float position_y = 300;
+    float PlayersSpeed = 550.0f;
+
+
     Texture playerTexture;
 
-    public Player() {
+    public Player(World world) {
+        BodyDef bodyDef = new BodyDef();
+        bodyDef.type = BodyDef.BodyType.DynamicBody;
+        bodyDef.position.set(position_x, position_y);
+
+        body = world.createBody(bodyDef);
+
+        playerShape = new PolygonShape();
+        playerShape.setAsBox(width / 2, height / 2);
+
+        fixtureDef = new FixtureDef();
+        fixtureDef.shape = playerShape;
+        fixtureDef.density = 0.5f;
+        fixtureDef.friction = 0.4f;
+        fixtureDef.restitution = 0.2f;
+        body.createFixture(fixtureDef);
         playerTexture = new Texture("Textures/player/Fall (32x32).png");
-        vector2 = new Vector2(position_x, position_y);
+
+
     }
 
 
     public void render(SpriteBatch batch) {
-        batch.draw(playerTexture, position_x, position_y);
-        if (Gdx.input.isKeyPressed(Input.Keys.A)) {
-            position_x -= Gdx.graphics.getDeltaTime() * PlayersSpeed;
+        batch.draw(playerTexture, position_x - width / 2, position_y - height / 2);
+        Vector2 vel = this.body.getLinearVelocity();
+        Vector2 pos = this.body.getPosition();
+        if (Gdx.input.isKeyPressed(Input.Keys.A) && vel.x > -MAX_VELOCITY) {
+            this.body.applyLinearImpulse(-0.80f, 0, pos.x, pos.y, true);
         }
         if (Gdx.input.isKeyPressed(Input.Keys.D)) {
             position_x += Gdx.graphics.getDeltaTime() * PlayersSpeed;
         }
         if (Gdx.input.isKeyPressed(Input.Keys.W)) {
             position_y += Gdx.graphics.getDeltaTime() * PlayersSpeed;
+            System.out.println(position_x + " " + position_y);
         }
-        if (Gdx.input.isKeyPressed(Input.Keys.S)) {
-            position_y -= 1f;
-        }
+          /*if (Gdx.input.isKeyPressed(Input.Keys.S)) {
+          *    position_y -= 1f;
+        } */
     }
 
     public void dispose() {
-        playerTexture = null;
+        playerTexture.dispose();
+        playerShape.dispose();
     }
 }
--- core/src/com/mygdx/game/MyGdxGame.java
+++ core/src/com/mygdx/game/MyGdxGame.java
@@ -10,16 +10,17 @@ import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.maps.tiled.TiledMap;
 import com.badlogic.gdx.maps.tiled.TmxMapLoader;
 import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
+import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.physics.box2d.*;
 import com.badlogic.gdx.utils.ScreenUtils;
 import com.mygdx.game.Level.Level_maker;
 import com.mygdx.game.Level.Player;
 
 public class MyGdxGame extends ApplicationAdapter {
     SpriteBatch batch;
-
     OrthographicCamera camera;
     OrthogonalTiledMapRenderer mapRender;
-
+    World world;
     TiledMap map;
     Level_maker level1;
     Player player;
@@ -27,7 +28,8 @@ public class MyGdxGame extends ApplicationAdapter {
     @Override
     public void create() {
         batch = new SpriteBatch();
-        player = new Player();
+        world = new World(new Vector2(0, -10), true);
+        player = new Player(world);
         level1 = new Level_maker("test.tmx");
         map = new TmxMapLoader().load("test.tmx");
         mapRender = new OrthogonalTiledMapRenderer(map);
@@ -40,14 +42,12 @@ public class MyGdxGame extends ApplicationAdapter {
     public void render() {
         ScreenUtils.clear(1, 1, 1, 1);
         batch.begin();
-        if (Gdx.input.isKeyPressed(Input.Keys.A)) {
-            player.keyDown(Input.Keys.A);
-        }
         player.render(batch);
         mapRender.setView(camera);
         camera.update();
         mapRender.render();
         batch.end();
+        world.step(1/60f,6,2);
     }
 
     @Override
